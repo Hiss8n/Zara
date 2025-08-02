fcd8b20d9b99bd6eb69dad3fc372063d49dd8920
Hissen Tutu <hissentutu@gmail.com>
2025-08-02 13:01:12 +0300

    verfiy bkn

--- backend/controllers/User.controller.js
+++ backend/controllers/User.controller.js
@@ -121,15 +121,16 @@ export const verify = async (req, res) => {
   try {
     const user = await User.findOne({
       verificationCode: otp,
+      verificationCodeExpiredAt: { $gt: new Date.now() }
       
     });
     if(user){
-      return res.status(400).json({success:false,message:"Wrong verification code!"})
+      return res.status(400).json({success:false,message:"Wrong verification code or expired"})
     }
-    
-    const expiredCode=await User.findOne({verificationCodeExpiredAt: { $gt: new Date.now() }})
+
+   /*  const expiredCode=await User.findOne({})
     if (!expiredCode){return res.status(40).json({ message: "Verification code Expired!" })
-    };
+    }; */
 
     user.verificationCode = undefined,
     user.verificationCodeExpiredAt = undefined;
